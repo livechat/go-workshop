1509d47ad72f36c1a3ef4fd5b3383208571a1e3a
Bartek Kubiak <b.kubiak@livechatinc.com>
2019-06-07 23:49:46 +0200

    .

--- handlers.go
+++ handlers.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"errors"
-	"log"
-)
-
 type handlers struct {
 	connections *connections
 	list        map[string]handler
@@ -24,46 +19,9 @@ func newHandlers(connections *connections) *handlers {
 }
 
 func (h *handlers) loginHandler(connection *connection, payload interface{}) (interface{}, error) {
-	pl, ok := payload.(*requestLogin)
-	if !ok {
-		log.Print("error: invalid login payload")
-		return nil, errors.New("internal")
-	}
-
-	if pl.Name == "" {
-		return nil, errors.New("`name` is required")
-	}
-
-	if pl.Avatar == "" {
-		return nil, errors.New("`avatar` is required")
-	}
-
-	// NO MUTEX ON PURPOSE
-	connection.name = pl.Name
-	connection.avatar = pl.Avatar
-
-	name := pl.Name
-	if err := h.connections.register(name, connection); err != nil {
-		return nil, err
-	}
-
-	h.connections.broadcastUsersDetails()
-
 	return nil, nil
 }
 
 func (h *handlers) broadcastHandler(connection *connection, payload interface{}) (interface{}, error) {
-	pl, ok := payload.(*requestBroadcast)
-	if !ok {
-		log.Print("error: invalid broadcast payload")
-		return nil, errors.New("internal")
-	}
-
-	if pl.Text == "" {
-		return nil, errors.New("`text` is required")
-	}
-
-	h.connections.broadcastText(connection.name, pl.Text)
-
 	return nil, nil
 }
